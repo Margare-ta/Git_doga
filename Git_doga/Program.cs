96a8b176334731e281b1f499783143a27e4a7bc2
Petrik Diak <diak@petrik.hu>
2024-01-31 13:09:32 +0100

    első feladat,első része

--- Git_doga/Program.cs
+++ Git_doga/Program.cs
@@ -10,6 +10,15 @@ namespace Git_doga
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Adj meg egy listát, hogy kilépj ebből a funkcióból, adj a listádhoz 0-át!");
+			int num = 1;
+			while (num != 0)
+			{
+				num = Convert.ToInt32(Console.ReadLine());
+			}
+			
+
+			Console.ReadKey();
 		}
 	}
 }
