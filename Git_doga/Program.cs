ace78becba281430dd589f8f0776df99fa564676
Petrik Diak <diak@petrik.hu>
2024-01-31 13:17:25 +0100

    Lista_feltoltes

--- Git_doga/Program.cs
+++ Git_doga/Program.cs
@@ -10,11 +10,16 @@ namespace Git_doga
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Adj meg egy listát, hogy kilépj ebből a funkcióból, adj a listádhoz 0-át!");
+			Console.WriteLine("Adj meg egy számokból álló listát, hogy kilépj ebből a funkcióból, adj a listádhoz 0-át!");
+			
 			int num = 1;
+
+			Reszletek r = new Reszletek();
+
 			while (num != 0)
 			{
 				num = Convert.ToInt32(Console.ReadLine());
+				r.Lista_feltoltes(num);
 			}
 			
 
---
+++ Git_doga/Reszletek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_doga
+{
+	internal class Reszletek
+	{
+		private List<int> nums = new List<int>();
+
+		
+
+		public void Lista_feltoltes(int num)
+		{
+			nums.Add(num);
+		}
+	}
+}
